df0fc6fddffc894c82a3f83193c8e5d9aaab36e5
Shariful Islam <sajukhan12905@gmail.com>
2025-07-17 19:47:43 +0600

    day 17

--- dsa.js
+++ dsa.js
@@ -18,10 +18,11 @@
 // console.log(minValue)
 
 const arr = [
-  42, 17, 89, 3, 56, 74, 28, 61, 9, 38, 92, 25, 7, 84, 46, 13, 70, 99, 31, 60,
+  42, 17, 89, 3, 56, 74, 28, 61, 9, 38, 92, 25, 7, 84, 46, 13,9, 70, 99, 31, 60,
+  -5, -83, 134,
 ];
 
-// function bubbleShort(arr) {
+// function bubbleSort(arr) {
 //   const n = arr.length;
 
 //   for (let i = 0; i < n - 1; i++) {
@@ -35,6 +36,75 @@ const arr = [
 //   }
 
 //   return arr
+// }
+
+// // function selectionSort(arr) {
+// //   const n = arr.length;
+
+// //   for (let i = 0; i < n; i++) {
+// //     let mindex = i;
+// //     for (let j = i + 1; j < n; j++) {
+// //       if (arr[mindex] > arr[j]) {
+// //         mindex = j;
+// //       }
+// //     }
+
+// //     if (mindex !== i) {
+// //       const temp = arr[i];
+// //       arr[i] = arr[mindex];
+// //       arr[mindex] = temp;
+// //     }
+// //   }
+
+// //   return arr
+// // }
+
+// function bubbleSort(arr) {
+//   const n = arr.length;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         const temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
+// function bubbleSort(arr) {
+//   const n = arr.length;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         const temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   return arr
+// }
+
+// function bubbleSort(arr) {
+//   const n = arr.length;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         const temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   return arr;
 // }
 
 function selectionSort(arr) {
@@ -55,7 +125,7 @@ function selectionSort(arr) {
     }
   }
 
-  return arr
+  return arr;
 }
 
 
